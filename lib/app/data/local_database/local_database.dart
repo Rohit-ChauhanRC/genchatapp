920d37ca5e0f8255748f1e02a0f4261b9ec8d066
user <user@Admin-2.local>
2024-12-04 10:12:26 +0530

    remove sqflite

--- lib/app/data/local_database/local_database.dart
+++ lib/app/data/local_database/local_database.dart
@@ -1,50 +1,50 @@
-import 'package:sqflite/sqflite.dart';
-import 'package:path/path.dart';
+// import 'package:sqflite/sqflite.dart';
+// import 'package:path/path.dart';
 
-import 'users_table.dart';
+// import 'users_table.dart';
 
-class DataBaseService {
-  Database? _database;
+// class DataBaseService {
+//   Database? _database;
 
-  Future<Database> get database async {
-    //
-    if (_database != null) {
-      return _database!;
-    }
+//   Future<Database> get database async {
+//     //
+//     if (_database != null) {
+//       return _database!;
+//     }
 
-    _database = await _initialize();
-    return _database!;
-  }
+//     _database = await _initialize();
+//     return _database!;
+//   }
 
-  Future<String> get fullPath async {
-    const name = 'genmakgenchat.db';
-    final path = await getDatabasesPath();
-    return join(path, name);
-  }
+//   Future<String> get fullPath async {
+//     const name = 'genmakgenchat.db';
+//     final path = await getDatabasesPath();
+//     return join(path, name);
+//   }
 
-  Future<Database> _initialize() async {
-    final path = await fullPath;
-    var database = await openDatabase(
-      path,
-      version: 1,
-      onCreate: create,
-      singleInstance: true,
-      onUpgrade: onUpgrade,
-    );
-    return database;
-  }
+//   Future<Database> _initialize() async {
+//     final path = await fullPath;
+//     var database = await openDatabase(
+//       path,
+//       version: 1,
+//       onCreate: create,
+//       singleInstance: true,
+//       onUpgrade: onUpgrade,
+//     );
+//     return database;
+//   }
 
-  Future<void> create(Database database, int verion) async {
-    await GenchatUserDB().createTable(database);
-    // await SellDB().createTable(database);
-    // await ProfileDB().createTable(database);
-  }
+//   Future<void> create(Database database, int verion) async {
+//     await GenchatUserDB().createTable(database);
+//     // await SellDB().createTable(database);
+//     // await ProfileDB().createTable(database);
+//   }
 
-  void onUpgrade(Database database, int oldVersion, int newVersion) async {
-    // MilkCollectionDB().onUpgrade(database, oldVersion, newVersion);
-    // VendorDB().onUpgrade(database, oldVersion, newVersion);
-    // ReceivingDB().onUpgrade(database, oldVersion, newVersion);
-    // SellDB().onUpgrade(database, oldVersion, newVersion);
-    // ProfileDB().onUpgrade(database, oldVersion, newVersion);
-  }
-}
+//   void onUpgrade(Database database, int oldVersion, int newVersion) async {
+//     // MilkCollectionDB().onUpgrade(database, oldVersion, newVersion);
+//     // VendorDB().onUpgrade(database, oldVersion, newVersion);
+//     // ReceivingDB().onUpgrade(database, oldVersion, newVersion);
+//     // SellDB().onUpgrade(database, oldVersion, newVersion);
+//     // ProfileDB().onUpgrade(database, oldVersion, newVersion);
+//   }
+// }

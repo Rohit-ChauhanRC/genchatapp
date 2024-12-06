954c5884078969af9cb7b446a340a66a356a4ee0
Anshul <anshulverma338@gmail.com>
2024-12-06 16:39:50 +0530

    //local search done in selectContact UI and controller...

--- lib/app/modules/select_contacts/views/select_contacts_view.dart
+++ lib/app/modules/select_contacts/views/select_contacts_view.dart
@@ -66,29 +66,29 @@ class SelectContactsView extends GetView<SelectContactsController> {
             children: [
               // Search Input
               TextFormField(
-                onChanged: (v) {},
+                onChanged: (value) => controller.searchQuery = value,
                 decoration: InputDecoration(
                   isDense: true,
                   filled: true,
                   fillColor: whiteColor,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    borderSide: BorderSide(color: textBarColor, width: 1),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    borderSide: BorderSide(
-                      color: textBarColor,
-                      width: 1,
-                    ), // Border for enabled state
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    borderSide: BorderSide(
-                      color: textBarColor,
-                      width: 2,
-                    ), // Border for focused state
-                  ),
+                  // border: OutlineInputBorder(
+                  //   borderRadius: BorderRadius.circular(20),
+                  //   borderSide: BorderSide(color: textBarColor, width: 1),
+                  // ),
+                  // enabledBorder: OutlineInputBorder(
+                  //   borderRadius: BorderRadius.circular(20),
+                  //   borderSide: BorderSide(
+                  //     color: textBarColor,
+                  //     width: 1,
+                  //   ), // Border for enabled state
+                  // ),
+                  // focusedBorder: OutlineInputBorder(
+                  //   borderRadius: BorderRadius.circular(20),
+                  //   borderSide: BorderSide(
+                  //     color: textBarColor,
+                  //     width: 2,
+                  //   ), // Border for focused state
+                  // ),
                   hintText: 'Search',
                   hintStyle: const TextStyle(
                     color: greyColor,
@@ -102,51 +102,58 @@ class SelectContactsView extends GetView<SelectContactsController> {
                 height: 10,
               ),
               Expanded(
-                child: Obx(() => controller.contacts.isNotEmpty &&
-                        controller.isContactRefreshed
-                    ? ListView.builder(
-                        itemCount: controller.contacts.length,
-                        itemBuilder: (context, i) {
-                          var contact = controller.contacts[i];
-                          return InkWell(
-                            onTap: () {
-                              // controller.selectContact(contact, context);
-                              Get.toNamed(Routes.SINGLE_CHAT,
-                                  arguments: contact.user);
-                            },
-                            child: Padding(
-                              padding: const EdgeInsets.only(bottom: 8.0),
-                              child: ListTile(
-                                title: Text(
-                                  contact.fullName,
-                                  style: const TextStyle(
-                                    fontSize: 18,
-                                  ),
+                child: Obx(() {
+                  if (!controller.isContactRefreshed) {
+                    return const Center(
+                      child: CircularProgressIndicator(color: textBarColor),
+                    );
+                  }
+                  var filteredContacts = controller.filteredContacts;
+                  if (filteredContacts.isEmpty) {
+                    return const Center(
+                      child: Text("No contacts found."),
+                    );
+                  }
+                  return ListView.builder(
+                      itemCount: filteredContacts.length,
+                      itemBuilder: (context, i) {
+                        var contact = filteredContacts[i];
+                        return InkWell(
+                          onTap: () {
+                            Get.toNamed(Routes.SINGLE_CHAT,
+                                arguments: contact.user);
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.only(bottom: 8.0),
+                            child: ListTile(
+                              title: Text(
+                                contact.fullName,
+                                style: const TextStyle(
+                                  fontSize: 18,
                                 ),
-                                leading: contact.image == null
-                                    ? const SizedBox()
-                                    : CircleAvatar(
-                                        backgroundImage:
-                                            MemoryImage(contact.image!),
-                                        radius: 30,
-                                      ),
-                                subtitle: Text(
-                                  contact.contactNumber.isNotEmpty
-                                      ? contact.contactNumber
-                                      : '',
-                                  style: const TextStyle(
-                                    fontSize: 14,
-                                  ),
+                              ),
+                              leading: contact.image == null
+                                  ? const CircleAvatar(
+                                child: Icon(Icons.person),
+                              )
+                              : CircleAvatar(
+                                backgroundImage:
+                                MemoryImage(contact.image!),
+                                radius: 30,
+                              ),
+                              subtitle: Text(
+                                contact.contactNumber.isNotEmpty
+                                    ? contact.contactNumber
+                                    : '',
+                                style: const TextStyle(
+                                  fontSize: 14,
                                 ),
                               ),
                             ),
-                          );
-                        })
-                    : const Center(
-                        child: CircularProgressIndicator(
-                          color: textBarColor,
-                        ),
-                      )),
+                          ),
+                        );
+                      });
+                }),
               ),
             ],
           ),

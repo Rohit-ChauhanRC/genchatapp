3c48038df560e43fe0b1dca9232f6d601a900efb
Anshul <anshulverma338@gmail.com>
2025-04-08 10:42:55 +0530

    //socket service file integrated....

---
+++ lib/app/config/services/socket_service.dart
@@ -0,0 +1,60 @@
+import 'package:get/get.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+
+class SocketService extends GetxService {
+  late IO.Socket _socket;
+  IO.Socket get socket => _socket;
+
+  final RxBool _isConnected = false.obs;
+  bool get isConnected => _isConnected.value;
+
+  Future<void> initSocket(String userId) async {
+    _socket = IO.io(
+      'http://app.maklife.in:10000',
+      IO.OptionBuilder()
+          .setTransports(['websocket'])
+          .disableAutoConnect()
+          .setQuery({'userId': userId})
+          .build(),
+    );
+
+    _socket.connect();
+
+    _socket.onConnect((_) {
+      print('✅ Socket connected');
+      _isConnected.value = true;
+    });
+
+    _socket.onDisconnect((_) {
+      print('❌ Socket disconnected');
+      _isConnected.value = false;
+    });
+
+    _socket.onError((data) {
+      print('⚠️ Socket error: $data');
+    });
+
+    // Add your custom events here
+    _socket.on('message-event', (data) {
+      print('📩 Message received: $data');
+      // TODO: Insert to local SQLite DB
+    });
+
+    _socket.on('message-acknowledgement', (data) {
+      print('✅ Message Ack: $data');
+      // TODO: Update message status in DB
+    });
+
+    _socket.on('custom-error', (data) {
+      print('🚫 Custom Error: $data');
+    });
+  }
+
+  void sendMessage(Map<String, dynamic> data) {
+    _socket.emit('message-event', data);
+  }
+
+  void disposeSocket() {
+    _socket.dispose();
+  }
+}

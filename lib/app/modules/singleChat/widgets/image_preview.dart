5f9938fe18eca2f0ab036395239fadcfddfcc5e6
Genmak <genmak@Genmaks-MacBook-Pro.local>
2025-08-06 13:35:55 +0530

    media issue fixed

--- lib/app/modules/singleChat/widgets/image_preview.dart
+++ lib/app/modules/singleChat/widgets/image_preview.dart
@@ -18,43 +18,42 @@ class ImagePreviewScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppColors.blackColor,
       appBar: AppBar(
-          backgroundColor: textBarColor,
-          iconTheme: const IconThemeData(color: Colors.white),
-          // automaticallyImplyLeading: false,
-          centerTitle: false,
-          title: const Text(
-            "Image Preview",
-            style: TextStyle(
-              fontSize: 20,
-              color: whiteColor,
-              fontWeight: FontWeight.bold,
-            ),
-          )),
-      body: Container(
-          margin: const EdgeInsets.all(20),
-          alignment: Alignment.center,
-          // width: Get.width * 0.7,
-          height: Get.height,
-          child: EasyImageView(
-            imageProvider: imageProvider,
-          )
-          // SizedBox.expand(
-          //   child: InteractiveViewer(
-          //     transformationController: _transformationController,
-          //     // boundaryMargin: const EdgeInsets.all(20.0),
-          //     // minScale: 0.1,
-          //     maxScale: 10,
-          //     // panAxis: PanAxis.aligned,
-          //     clipBehavior: Clip.antiAliasWithSaveLayer,
-          //     // maxScale: 3,
-          //     alignment: Alignment.center,
-          //     // constrained: true,
-          //     child: GestureDetector(
-          //       onTap: (){},
-          //       child: Image.file(File(imagePath))),
-          //   ),
-          // ),
+        backgroundColor: textBarColor,
+        iconTheme: const IconThemeData(color: Colors.white),
+        // automaticallyImplyLeading: false,
+        centerTitle: false,
+        title: const Text(
+          "Image Preview",
+          style: TextStyle(
+            fontSize: 20,
+            color: whiteColor,
+            fontWeight: FontWeight.bold,
           ),
+        ),
+      ),
+      body: Container(
+        // margin: const EdgeInsets.all(20),
+        alignment: Alignment.center,
+        // width: Get.width * 0.7,
+        height: Get.height,
+        child: EasyImageView(imageProvider: imageProvider),
+        // SizedBox.expand(
+        //   child: InteractiveViewer(
+        //     transformationController: _transformationController,
+        //     // boundaryMargin: const EdgeInsets.all(20.0),
+        //     // minScale: 0.1,
+        //     maxScale: 10,
+        //     // panAxis: PanAxis.aligned,
+        //     clipBehavior: Clip.antiAliasWithSaveLayer,
+        //     // maxScale: 3,
+        //     alignment: Alignment.center,
+        //     // constrained: true,
+        //     child: GestureDetector(
+        //       onTap: (){},
+        //       child: Image.file(File(imagePath))),
+        //   ),
+        // ),
+      ),
     );
   }
 }

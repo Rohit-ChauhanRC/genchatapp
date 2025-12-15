aad2aa5a4b22ee8729b75fd7615d9d3e3ab1994b
Genmak <genmak@Genmaks-MacBook-Pro.local>
2025-12-15 15:19:15 +0530

    dev

--- lib/main.dart
+++ lib/main.dart
@@ -95,7 +95,7 @@ void main() async {
     print('Notification permission not determined');
   }
 
-  AppConfig.setEnvironment(AppEnvironment.prod);
+  AppConfig.setEnvironment(AppEnvironment.dev);
 
   runApp(const App());
 }
